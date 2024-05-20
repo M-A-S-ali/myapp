97156c6281aed015c6ca9dd3d587cd67b9856227
M-A-S-ali <abdulsabooralim@gmail.com>
2024-05-20 15:05:09 -0400

    second-commit 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/Accordian/data.js 	new file:   src/components/Accordian/index.jsx 	new file:   src/components/Accordian/styles.css 	new file:   src/components/Github-profile-finder/index.jsx 	new file:   src/components/Github-profile-finder/png/github.png 	new file:   src/components/Github-profile-finder/styles.css 	new file:   src/components/Github-profile-finder/user.jsx 	new file:   src/components/Image Slider/index.jsx 	new file:   src/components/Image Slider/styles.css 	new file:   src/components/Load-more-data/index.jsx 	new file:   src/components/Load-more-data/styles.css 	new file:   src/components/Random color/index.jsx 	new file:   src/components/Scroll-indicator/index.jsx 	new file:   src/components/Scroll-indicator/scroll.css 	new file:   src/components/Star Rating/index.jsx 	new file:   src/components/Star Rating/styles.css 	new file:   src/components/Tree-view/data.js 	new file:   src/components/Tree-view/index.jsx 	new file:   src/components/Tree-view/menu-item.jsx 	new file:   src/components/Tree-view/menu-list.jsx 	new file:   src/components/Tree-view/styles.css 	new file:   src/components/custom-model-popup/modal-test.jsx 	new file:   src/components/custom-model-popup/modal.css 	new file:   src/components/custom-model-popup/modal.jsx 	new file:   src/components/custom-tabs/tab-test.jsx 	new file:   src/components/custom-tabs/tabs.css 	new file:   src/components/custom-tabs/tabs.jsx 	new file:   src/components/ligth-dark-mode/index.jsx 	new file:   src/components/ligth-dark-mode/theme.css 	new file:   src/components/ligth-dark-mode/useLocalStrorage.jsx 	new file:   src/components/qr-code-generator/index.jsx 	new file:   src/components/qr-code-generator/styles.css 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordian from "./components/Accordian";
+import ImageSlider from "./components/Image Slider";
+import LoadMoreData from "./components/Load-more-data";
+import RandomColor from "./components/Random color";
+import ScrollIndicator from "./components/Scroll-indicator";
+import StarRating from "./components/Star Rating";
+import TreeView from "./components/Tree-view";
+import menus from "./components/Tree-view/data";
+import LightDarkMode from "./components/ligth-dark-mode";
+import QRCodeGenerator from "./components/qr-code-generator";
+import TabTest from './components/custom-tabs/tab-test';
+import ModalTest from "./components/custom-model-popup/modal-test";
+import GithubProfileFinder from "./components/Github-profile-finder";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Accordian/>
+      <RandomColor/>
+      <StarRating noOfStars={10}/>
+      <ImageSlider url={"https://picsum.photos/v2/list"} limit={15} page={10} />
+      <LoadMoreData/>
+      <TreeView menus={menus}/>
+      <QRCodeGenerator />
+      <LightDarkMode />
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"}/>
+      <TabTest />
+      <ModalTest/> 
+      <GithubProfileFinder/>
+
     </div>
   );
 }
---
+++ src/components/Tree-view/menu-item.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import MenuList from "./menu-list";
+import {FaMinus,FaPlus} from 'react-icons/fa'
+
+
+export default function MenuItem({item}){
+
+    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+
+    function handleToggleChildren(getCurrentlabel){
+        setDisplayCurrentChildren({
+            ...displayCurrentChildren,
+            [getCurrentlabel] : !displayCurrentChildren[getCurrentlabel]
+        })
+
+    }
+
+    return <li>
+        <div className="menu-item">
+        <p>{item.label}</p>
+        {
+            item && item.children && item.children.length ? <span onClick={()=>handleToggleChildren(item.label)}>
+                {
+                    displayCurrentChildren[item.label] ? <FaMinus color="whiite" size={25}/> : <FaPlus color="white" size={25}/>
+                }
+            </span> : null
+        }
+        </div>
+
+        {
+            item && item.children && item.children.length > 0 && displayCurrentChildren[item.label]? 
+            (<MenuList list={item.children} />)
+            : null
+        }
+    </li>
+}
